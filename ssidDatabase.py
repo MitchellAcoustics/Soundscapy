836241e53d0ba7066e8e14b35ea5c127c477283d
Andrew Mitchell <mitchellacoustics15@gmail.com>
2021-01-21 21:11:30 +0000

    add function to collect wav directories

--- ssidDatabase.py
+++ ssidDatabase.py
@@ -38,14 +38,15 @@ LOCATION_IDS = {
 IGNORE_LIST = ["AllLondon", "AllyPally", "CoventGd1", "OxfordSt"]
 
 
-def collect_param_dirs(
+def collect_all_dirs(
     root_directory: Path,
     location_ids: list,
     param_list: list,
     include_TS: bool = True,
     include_spectrum: bool = True,
+    include_WAV: bool = True,
 ):
-    """Iterate throughout the SSID DB file structure to extract TimeSeries, SpectrumData fil paths.
+    """Iterate throughout the SSID DB file structure to extract TimeSeries, SpectrumData, WAV directory paths.
 
     Parameters
     ----------
@@ -56,19 +57,21 @@ def collect_param_dirs(
     param_list : list
         A subset of parameters to include in the filepath collection
     include_TS : bool, optional
-        Collect TimeSeries files?, by default True
+        Collect TimeSeries dirs?, by default True
     include_spectrum : bool, optional
-        Collect SpectrumData files?, by default True
+        Collect SpectrumData dirs?, by default True
+    include_WAV : bool, optional
+        Collect WAV dirs?, by default True
 
     Returns
     -------
     tuple of lists
-        A tuple containing the full lists of TimeSeries and SpectrumData files.
+        A tuple containing the full lists of TimeSeries, SpectrumData, and WAV directories.
         These lists contain WindowsPath objects.
     
     Examples
     ________
-    >>> full_ts_list, full_spectrum_list = collect_param_dirs(TEST_DIR, LOCATION_IDS["Test"], PARAM_LIST)
+    >>> full_ts_list, full_spectrum_list, full_wav_list = collect_all_dirs(TEST_DIR, LOCATION_IDS["Test"], PARAM_LIST)
     >>> len(full_ts_list)
     33
     >>> len(full_spectrum_list)
@@ -108,15 +111,22 @@ def collect_param_dirs(
     if include_TS:
         # Collect Time Series parameter directories
         full_ts_list = _ts_dirs(bin_dirs, param_list)
+    else:
+        full_ts_list = []
 
     if include_spectrum:
         # Collect Spectrum directories
-
         full_spectrum_list = _spectrum_dirs(bin_dirs)
+    else:
+        full_spectrum_list = []
 
-    # TODO: Add include_wav
+    if include_WAV:
+        # Collect WAV directories
+        full_wav_list = _wav_dirs(bin_dirs)
+    else:
+        full_wav_list = []
 
-    return full_ts_list, full_spectrum_list
+    return full_ts_list, full_spectrum_list, full_wav_list
 
 
 def _spectrum_dirs(bin_dirs):
@@ -164,8 +174,23 @@ def _ts_dirs(bin_dirs, param_list):
     return full_ts_list
 
 
+def _wav_dirs(bin_dirs):
+    # Collect Wav directories
+    wav_dirs = []
+    for directory in bin_dirs:
+        wav_dir = [
+            child
+            for child in directory.iterdir()
+            if child.is_dir() and "WAV" in child.name
+        ][0]
+        wav_dirs.append(wav_dir)
+
+    return wav_dirs
+
+
 if __name__ == "__main__":
     import doctest
 
     TEST_DIR = Path("test_DB")
     doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
+
--- test_database.py
+++ test_database.py
@@ -8,10 +8,10 @@ TEST_DIR = Path("test_DB")
 def test_input_sanity():
     # Does the function raise appropriate input errors?
     with raises(TypeError) as exception:
-        db.collect_param_dirs(["test"], ["LocationA"], ["LevelA"])
+        db.collect_all_dirs(["test"], ["LocationA"], ["LevelA"])
     with raises(FileNotFoundError) as exception:
         bad_path = Path("testy")
-        db.collect_param_dirs(bad_path, ["LocationA"], ["LevelA"])
+        db.collect_all_dirs(bad_path, ["LocationA"], ["LevelA"])
 
 
 def test_spectrum_dirs_collection():
@@ -47,3 +47,18 @@ def test_ts_dirs_collection():
     full_ts_list = db._ts_dirs(bin_dirs, param_list)
     assert len(full_ts_list) == 6
 
+
+def test_wav_dirs_collection():
+    bin_dirs = [
+        TEST_DIR.joinpath(
+            "OFF_LocationA1_FULL_2020-12-31", "OFF_LocationA1_BIN_2020-12-31"
+        ),
+        TEST_DIR.joinpath(
+            "OFF_LocationA2_FULL_2021-01-01", "OFF_LocationA2_BIN_2021-01-01"
+        ),
+        TEST_DIR.joinpath(
+            "OFF_LocationB1_FULL_2021-01-13", "OFF_LocationB1_BIN_2021-01-13"
+        ),
+    ]
+    full_wav_list = db._wav_dirs(bin_dirs)
+    assert len(full_wav_list) == 3
