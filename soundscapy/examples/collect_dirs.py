d502f32d8c748c560822d49940f67dfcfac04a11
MitchellAcoustics <mitchellacoustics15@gmail.com>
2021-01-28 18:44:20 +0000

    add collect_dirs example

---
+++ soundscapy/examples/collect_dirs.py
@@ -0,0 +1,17 @@
+import sys, os
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + "/../../")
+
+from pathlib import Path
+from soundscapy.ssid.database import collect_all_dirs
+from soundscapy.ssid.parameters import (
+    PARAM_LIST,
+    LOCATION_IDS,
+    IGNORE_LIST,
+    CATEGORISED_VARS,
+)
+
+DATA_DIR = Path("R:\\UCL_SSID\\SSID_DATABASE\\SSID_London")
+ts_dirs, spectrum_dirs, wav_dirs = collect_all_dirs(DATA_DIR, LOCATION_IDS['London'], PARAM_LIST, include_TS=False, include_spectrum=False, include_WAV=True)
+wav_dirs
--- soundscapy/ssid/parameters.py
+++ soundscapy/ssid/parameters.py
@@ -78,37 +78,119 @@ CATEGORISED_VARS = {
     "misc": ["AnythingElse"],
 }
 
-category_levels = {
+ssid_v1_lvls = {
     "sound_source_dominance": {
-        1: "Not at all",
-        2: "A little",
-        3: "Moderately",
-        4: "A lot",
-        5: "Dominates completely",
+        "type": "Likert",
+        "levels": {
+            1: "Not at all",
+            2: "A little",
+            3: "Moderately",
+            4: "A lot",
+            5: "Dominates completely",
+        },
     },
     "PAQ": {
-        1: "Strongly disagree",
-        2: "Somewhat disagree",
-        3: "Neither agree nor disagree",
-        4: "Somewhat agree",
-        5: "Strongly agree",
+        "type": "Likert",
+        "levels": {
+            1: "Strongly disagree",
+            2: "Somewhat disagree",
+            3: "Neither agree nor disagree",
+            4: "Somewhat agree",
+            5: "Strongly agree",
+        },
     },
     "overall_soundscape": {
-        1: "Strongly disagree",
-        2: "Somewhat disagree",
-        3: "Neither agree nor disagree",
-        4: "Somewhat agree",
-        5: "Strongly agree",
+        "type": "Likert",
+        "levels": {
+            1: "Strongly disagree",
+            2: "Somewhat disagree",
+            3: "Neither agree nor disagree",
+            4: "Somewhat agree",
+            5: "Strongly agree",
+        },
     },
     "who": {
-        0: "At no time",
-        1: "Some of the time",
-        2: "Less than half of the time",
-        3: "More than half of the time",
-        4: "Most of the time",
-        5: "All of the time",
+        "type": "ordinal",
+        "levels": {
+            0: "At no time",
+            1: "Some of the time",
+            2: "Less than half of the time",
+            3: "More than half of the time",
+            4: "Most of the time",
+            5: "All of the time",
+        },
     },
 }
+
+ssid_v1_aliases = {
+    "GroupID": ["group_id"],
+    "SessionID": ["session_id"],
+    "LocationID": ["location_id"],
+    "RecordID": ["record_id"],
+    "Traffic": ["ssi01"],
+    "Other": ["ssi02"],
+    "Human": ["ssi03"],
+    "Natural": ["ssi04"],
+    "pleasant": ["paq01", "pl"],
+    "chaotic": ["paq02", "ch"],
+    "vibrant": ["paq03", "vi", "vib"],
+    "uneventful": ["paq04", "un"],
+    "calm": ["paq05", "ca"],
+    "annoying": ["paq06", "an"],
+    "monotonous": ["paq08", "mo", "mono"],
+    "overall": ["sss01"],
+    "appropriateness": ["sss02", "appropriate", "approp"],
+    "loudness": ["sss03"],
+    "often": ["sss04"],
+    "visit_again": ["sss05"],
+    "who01": ["who1"],
+    "who02": ["who2"],
+    "who03": ["who3"],
+    "who04": ["who4"],
+    "who05": ["who5"],
+    "WHO_sum": ["who"],
+    "Age": ["age00"],
+    "Gender": ["gen", "gen00"],
+    "Occupation": ["occ00", "occup", "occ"],
+    "Education": ["edu00", "edu"],
+    "Ethnicity": ["eth00", "eth"],
+    "Resid": ["misc00", "Resident"],
+    "AnythingElse": ["misc01"],
+}
+
+
+class SurveyQ:
+    def __init__(
+        self,
+        label,
+        category,
+        version="ssid_v1",
+        aliases=None,
+        type=None,
+        levels=None,
+        text="",
+    ):
+        self.label = label
+        self.category = category
+        self.text = text
+
+        if category in ssid_v1_lvls.keys() and levels is None:
+            self.levels = ssid_v1_lvls[category]["levels"]
+            self.type = ssid_v1_lvls[category]["type"]
+        else:
+            self.levels = levels
+            if type is None:
+                raise AttributeError(
+                    "Need to define a type if self-defining the levels"
+                )
+            else:
+                self.type = type
+
+        if label in ssid_v1_aliases.keys() and aliases is None:
+            self.aliases = ssid_v1_aliases[label]
+        else:
+            self.aliases = aliases
+
 
 # ? Add a 'type' tag? i.e. "type": "Likert" or "type": "Ordinal"
 SURVEY_VARS = {
@@ -118,104 +200,104 @@ SURVEY_VARS = {
     "RecordID": {"aliases": ["record_id"],},
     "Traffic": {
         "aliases": ["ssi01"],
-        "levels": category_levels["sound_source_dominance"],
+        "levels": ssid_v1_lvls["sound_source_dominance"],
         "text": "Traffic noise (e.g. cars, buses, trains, airplanes)",
     },
     "Other": {
         "aliases": ["ssi02"],
-        "levels": category_levels["sound_source_dominance"],
+        "levels": ssid_v1_lvls["sound_source_dominance"],
         "text": "Other noise (e.g. sirens, construction, industry, loadining of goods)",
     },
     "Human": {
         "aliases": ["ssi03"],
-        "levels": category_levels["sound_source_dominance"],
+        "levels": ssid_v1_lvls["sound_source_dominance"],
         "text": "Sounds from human beings (e.g. conversation, laughter, children at play, footsteps)",
     },
     "Natural": {
         "aliases": ["ssi04"],
-        "levels": category_levels["sound_source_dominance"],
+        "levels": ssid_v1_lvls["sound_source_dominance"],
         "text": "Natural sounds (e.g. singing birds, flowing water, wind in vegetation)",
     },
     "pleasant": {
         "aliases": ["paq01", "pl"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Pleasant",
     },
     "chaotic": {
         "aliases": ["paq02", "ch"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Chaotic",
     },
     "vibrant": {
         "aliases": ["paq03", "vi", "vib"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Vibrant",
     },
     "uneventful": {
         "aliases": ["paq04", "un"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Uneventful",
     },
-    "calm": {"aliases": ["paq05", "ca"], "levels": category_levels["PAQ"],},
-    "annoying": {"aliases": ["paq06", "an"], "levels": category_levels["PAQ"],},
+    "calm": {"aliases": ["paq05", "ca"], "levels": ssid_v1_lvls["PAQ"],},
+    "annoying": {"aliases": ["paq06", "an"], "levels": ssid_v1_lvls["PAQ"],},
     "eventful": {
         "aliases": ["paq07", "ev"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Eventful",
     },
     "monotonous": {
         "aliases": ["paq08", "mo", "mono"],
-        "levels": category_levels["PAQ"],
+        "levels": ssid_v1_lvls["PAQ"],
         "text": "Monotonous",
     },
     "overall": {
         "aliases": ["sss01"],
-        "levels": category_levels["overall_soundscape"],
+        "levels": ssid_v1_lvls["overall_soundscape"],
         "text": "Overall, how would you descrive the present surrounding sound environment?",
     },
     "appropriateness": {
         "aliases": ["sss02", "appropriate", "approp"],
-        "levels": category_levels["overall_soundscape"],
+        "levels": ssid_v1_lvls["overall_soundscape"],
         "text": "Overall, to what extent is the present surrounding sound environment approrpriate to the present place?",
     },
     "loudness": {
         "aliases": ["sss03"],
-        "levels": category_levels["overall_soundscape"],
+        "levels": ssid_v1_lvls["overall_soundscape"],
         "text": "How loud would you say the sound environment is?",
     },
     "often": {
         "aliases": ["sss04"],
-        "levels": category_levels["overall_soundscape"],
+        "levels": ssid_v1_lvls["overall_soundscape"],
         "text": "How often do you visit this place?",
     },
     "visit_again": {
         "aliases": ["sss05"],
-        "levels": category_levels["overall_soundscape"],
+        "levels": ssid_v1_lvls["overall_soundscape"],
         "text": "How often would you like to visit this place again?",
     },
     "who01": {
         "aliases": ["WHO01"],
-        "levels": category_levels["who"],
+        "levels": ssid_v1_lvls["who"],
         "text": "I have felt cheerful and in good spirits.",
     },
     "who02": {
         "aliases": ["WHO02"],
-        "levels": category_levels["who"],
+        "levels": ssid_v1_lvls["who"],
         "text": "I have felt calm and relaxed.",
     },
     "who03": {
         "aliases": ["WHO03"],
-        "levels": category_levels["who"],
+        "levels": ssid_v1_lvls["who"],
         "text": "I have felt active and vigorous.",
     },
     "who04": {
         "aliases": ["WHO04"],
-        "levels": category_levels["who"],
+        "levels": ssid_v1_lvls["who"],
         "text": "I woke up feeling fresh and rested.",
     },
     "who05": {
         "aliases": ["WHO05"],
-        "levels": category_levels["who"],
+        "levels": ssid_v1_lvls["who"],
         "text": "My daily life has been filled with things that interest me.",
     },
     "WHO_sum": {"aliases": ["WHO_SUM", "who"], "range": (1, 100),},
