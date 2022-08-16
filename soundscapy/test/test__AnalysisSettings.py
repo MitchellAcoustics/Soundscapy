1eb5ee413992d99224784333f5ef8b3ee4e881cc
Andrew Mitchell <mitchellacoustics15@gmail.com>
2022-08-16 16:17:15 +0100

    started adding database tests

---
+++ soundscapy/test/test__AnalysisSettings.py
@@ -0,0 +1,20 @@
+import pytest
+from soundscapy import AnalysisSettings
+
+
+def test_default():
+    assert isinstance(AnalysisSettings.default(), dict)
+    assert list(AnalysisSettings.default().keys()) == [
+        "PythonAcoustics",
+        "MoSQITo",
+        "scikit-maad",
+        "runtime",
+    ]
+
+
+def test_from_yaml():
+    assert isinstance(AnalysisSettings.from_yaml("../default_settings.yaml"), dict)
+    assert (
+        AnalysisSettings.from_yaml("../default_settings.yaml")
+        == AnalysisSettings.default()
+    )
