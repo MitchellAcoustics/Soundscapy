ced5534b8892443120cdd30018177d0ebc76fbe2
Andrew Mitchell <mitchellacoustics15@gmail.com>
2023-09-09 13:44:29 +0100

    Docs (#72)
    * Try to fix sphinx_rtd_theme for readthedocs build
    
    * Try to fix sphinx_rtd_theme for readthedocs build
    
    Signed-off-by: Andrew Mitchell <mitchellacoustics15@gmail.com>
    
    * Try to fix sphinx_rtd_theme for readthedocs build pt3
    
    Signed-off-by: Andrew Mitchell <mitchellacoustics15@gmail.com>
    
    ---------
    
    Signed-off-by: Andrew Mitchell <mitchellacoustics15@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 # Problems with imports? Could try `export PYTHONPATH=$PYTHONPATH:`pwd`` from root project dir...
 import os
 import sys
-from importlib.machinery import SourceFileLoader
 
 sys.path.insert(0, os.path.abspath(".."))  # Source code dir relative to this file
 
@@ -44,6 +43,7 @@ extensions = [
     # 'sphinx_autodoc_typehints',  # Automatically document param types (less noise in class signature)
     "nbsphinx",  # Integrate Jupyter Notebooks and Sphinx
     "IPython.sphinxext.ipython_console_highlighting",
+    "sphinx_rtd_theme",
 ]
 
 # Mappings for sphinx.ext.intersphinx. Projects have to have Sphinx-generated doc! (.inv file)
@@ -116,8 +116,9 @@ on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
 
-    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme = "sphinx_rtd_theme"
 html_css_files = ["readthedocs-custom.css"]  # Override some CSS settings
 html_logo = "logo/DarkLogo.png"
 
