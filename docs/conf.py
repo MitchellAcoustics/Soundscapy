ff620debcf55d7a4f7307161a685e291364b83a2
Andrew Mitchell <mitchellacoustics15@gmail.com>
2023-09-09 14:14:27 +0200

    Try to fix sphinx_rtd_theme for readthedocs build
    Signed-off-by: Andrew Mitchell <mitchellacoustics15@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 # Problems with imports? Could try `export PYTHONPATH=$PYTHONPATH:`pwd`` from root project dir...
 import os
 import sys
-from importlib.machinery import SourceFileLoader
 
 sys.path.insert(0, os.path.abspath(".."))  # Source code dir relative to this file
 
@@ -116,8 +115,9 @@ on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
 
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_css_files = ["readthedocs-custom.css"]  # Override some CSS settings
 html_logo = "logo/DarkLogo.png"
 
