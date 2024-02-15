15d574fd9b7cee383d00b459eee3ea0450a43c75
Andrew Mitchell <mitchellacoustics15@gmail.com>
2024-02-15 23:37:04 +0000

    remove deprecated isd accessor
    Signed-off-by: Andrew Mitchell <mitchellacoustics15@gmail.com>

--- soundscapy/__init__.py
+++ soundscapy/__init__.py
@@ -4,10 +4,10 @@ Soundscapy is a Python library for soundscape analysis and visualisation.
 
 import importlib.metadata
 
+from soundscapy import plotting
 from soundscapy.analysis._AnalysisSettings import AnalysisSettings, get_default_yaml
 from soundscapy.analysis._Binaural import Binaural
-from soundscapy.utils import _sspy_accessor, surveys
-from soundscapy.databases import isd, araus, satp
-from soundscapy import plotting
+from soundscapy.databases import araus, isd, satp
+from soundscapy.utils import surveys
 
 __version__ = importlib.metadata.version("soundscapy")
--- soundscapy/plotting/__init__.py
+++ soundscapy/plotting/__init__.py
@@ -2,3 +2,4 @@
 The module ``plotting`` contains functions for plotting soundscape circumplex data and directly plotting Likert scale data.
 """
 from soundscapy.plotting.circumplex import scatter, density, jointplot
+from soundscapy.plotting import likert
