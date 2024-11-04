db1c16b2f5fb4bb30f934d6b4d055f724e09dfa8
Andrew Mitchell <a.j.mitchell@ucl.ac.uk>
2024-11-04 17:39:58 +0000

    run ruff

--- src/soundscapy/logging.py
+++ src/soundscapy/logging.py
@@ -111,6 +111,7 @@ def is_notebook() -> bool:
 
     try:
         from IPython import get_ipython
+
         shell = get_ipython().__class__.__name__
         if shell == "ZMQInteractiveShell":  # Jupyter notebook/lab
             return True
