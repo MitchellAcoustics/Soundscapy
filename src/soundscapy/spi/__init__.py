e620fee547a00e3e1d2c6930b12f653de247f9a3
Andrew Mitchell <a.j.mitchell@ucl.ac.uk>
2025-10-02 02:20:24 +0100

    feat: add warnings for experimental SATP and SPI analysis modules
    Signed-off-by: Andrew Mitchell <a.j.mitchell@ucl.ac.uk>

--- src/soundscapy/spi/__init__.py
+++ src/soundscapy/spi/__init__.py
@@ -5,6 +5,8 @@ This module provides functions and classes for calculating SPI,
 based on the R implementation. Requires optional dependencies.
 """
 
+import warnings
+
 # Check for required dependencies directly
 # This will raise ImportError if any dependency is missing
 try:
@@ -37,3 +39,6 @@ __all__ = [
     "msn",
     "spi_score",
 ]
+
+msg = "The SPI analysis module is experimental. Use with caution."
+warnings.warn(msg, UserWarning, stacklevel=2)
